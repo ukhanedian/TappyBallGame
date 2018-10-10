237a93afbaba1bec124200f390474f8ac1864deb
Usman Khan <ukhan.nedian@gmail.com>
2018-10-10 11:16:51 +0500

    Tappy Ball Menu and UI

---
+++ Assets/MenuUiManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Use anything related to scene management
+
+public class MenuUiManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play () { // Takes our game to the play scene whenever the button is clicked
+
+        SceneManager.LoadScene ("level1");
+    }
+}
