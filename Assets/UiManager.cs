3cda045c798faa2eeb976616ace38ec37cdc0d98
Usman Khan <ukhan.nedian@gmail.com>
2018-10-30 14:21:20 +0500

    Adding Attractive User Interface Elements

--- Assets/UiManager.cs
+++ Assets/UiManager.cs
@@ -12,8 +12,17 @@ public class UiManager : MonoBehaviour {
     // Create a reference to gameOverPanel
     public GameObject gameOverPanel;
 
+    // Create a reference to startUI
+    public GameObject startUI;
+
+    // Create a reference to GameOver Text
+    public GameObject gameOverText;
+
     // Make some public variables to assign Ui elements
     public Text scoreText;
+
+    // Create a reference to highScore Text
+    public Text highScoreText;
 
     void Awake () {
 
@@ -36,9 +45,16 @@ public class UiManager : MonoBehaviour {
         Debug.Log (ScoreManager.instance.score);
     }
 
+    // function for game in play
+    public void GameStart () {
+
+        startUI.SetActive (false);
+    }
+
     // function for gameOver animation
     public void GameOver () {
 
+        highScoreText.text = PlayerPrefs.GetInt ("Highscore").ToString();
         gameOverPanel.SetActive (true);
     }
 
